6a8f2277894f165e4ecc01555c3ebb52d0fed256
Shourya Eaga <shourya.eaga.09@gmail.com>
2026-09-16 13:21:40 +0100

    day 3

--- Optics/Optics.py
+++ Optics/Optics.py
@@ -12,13 +12,26 @@ def reflection_vector(incident_vector, normal_vector):
 
 # Snell's law in vector form
 def snells_law(n1, n2, incident_vector, normal_vector):
+    # check for invalid refractive indices
+    if n1 <= 0 or n2 <= 0:
+        raise ValueError("Refractive indices must be positive.")
+    # check for zero-length vectors
+    if np.linalg.norm(normal_vector) == 0 or np.linalg.norm(incident_vector) == 0:
+        raise ValueError("Normal vector cannot be zero-length.")
+
     # find the ratio between the refractive indices
     mu = n1/n2
     # find angle between the incident vector and the normal vector
     cos_theta_i = -np.dot(normal_vector, incident_vector)
 
+    # calcualate the square root term to check for total internal reflection
+    square_root_term = 1 - mu**2 * (1 - cos_theta_i**2)
+    if square_root_term < 0: # indicates total internal reflection
+        # calculate the reflection vector and return it.
+        return reflection_vector(incident_vector, normal_vector)
+        
     # find transmitted vector using Snell's law in vector form
-    transmitted_vector = math.sqrt(1-mu**2 * (1 - cos_theta_i**2)) * normal_vector + mu * incident_vector
+    transmitted_vector = mu*incident_vector + (mu * cos_theta_i - math.sqrt(square_root_term)) * normal_vector
 
     # return the vector result
     return transmitted_vector
--- Optics/Optics_test.py
+++ Optics/Optics_test.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from Optics import snells_law
+from Optics import snells_law, reflection_vector
 
 
 class TestOptics(unittest.TestCase):
@@ -48,25 +48,11 @@ class TestOptics(unittest.TestCase):
             n1, n2, incident_vector, normal_vector
         )
 
-        self.assertAlmostEqual(transmitted_vector[0], 1.0, places=5)
-        self.assertAlmostEqual(transmitted_vector[1], 0.0, places=5)
+        self.assertAlmostEqual(transmitted_vector[0], 0.666666, places=5)
+        self.assertAlmostEqual(transmitted_vector[1], -0.745356, places=5)
 
 
-        # Test 4: Boundary / approximately critical angle
-        n1 = 1.5
-        n2 = 1.0
-        incident_vector = np.array([0.667, -0.745])
-        normal_vector = np.array([0, 1])
-
-        transmitted_vector = snells_law(
-            n1, n2, incident_vector, normal_vector
-        )
-
-        self.assertAlmostEqual(transmitted_vector[0], 1.0, places=2)
-        self.assertAlmostEqual(transmitted_vector[1], 0.0, places=2)
-
-
-        # Test 5: Erroneous / invalid refractive index
+        # Test 4: Erroneous / invalid refractive index
         # This test is to make sure that invalid data raises an error, which can be handled
         # an error is raised so that elsewhere in the program, the error can be handled and the program can continue to run without crashing
 
@@ -81,7 +67,7 @@ class TestOptics(unittest.TestCase):
             )
 
 
-        # Test 6: Erroneous / zero-length normal vector
+        # Test 5: Erroneous / zero-length normal vector
         # This test is to make sure that invalid data raises an error, which can be handled
         # an error is raised so that elsewhere in the program, the error can be handled and the program can continue to run without crashing
         n1 = 1.0
@@ -92,6 +78,59 @@ class TestOptics(unittest.TestCase):
         with self.assertRaises(ValueError):
             snells_law(
                 n1, n2, incident_vector, normal_vector
+            )
+
+    def test_reflection_vector(self):
+        # Test 1: Normal reflection
+        # create input vectors from the test plan
+        incident_vector = np.array([0.707107, -0.707107])
+        normal_vector = np.array([0, 1])
+        # use the subroutine defined the in the Optics.py file to find the reflected vector
+        reflected_vector = reflection_vector(
+            incident_vector, normal_vector
+        )
+        # check the output is as expected
+        self.assertAlmostEqual(reflected_vector[0], 0.707107, places=5)
+        self.assertAlmostEqual(reflected_vector[1], 0.707107, places=5)
+
+        # this is then copied for all the other tests, 
+        # with their corresponding inputs and outputs from the test plan
+
+        # Test 2: Angled reflection
+        incident_vector = np.array([-0.5, -0.866025])
+        normal_vector = np.array([0, 1])
+
+        reflected_vector = reflection_vector(
+            incident_vector, normal_vector
+        )
+
+        self.assertAlmostEqual(reflected_vector[0], -0.5, places=5)
+        self.assertAlmostEqual(reflected_vector[1], 0.866025, places=5)
+
+
+        # Test 3: Boundary / grazing incidence
+        incident_vector = np.array([1, 0])
+        normal_vector = np.array([0, 1])
+
+        reflected_vector = reflection_vector(
+            incident_vector, normal_vector
+        )
+
+        self.assertAlmostEqual(reflected_vector[0], 1.0, places=5)
+        self.assertAlmostEqual(reflected_vector[1], 0.0, places=5)
+
+
+        # Test 4: Erroneous / zero-length normal vector
+        incident_vector = np.array([0, -1])
+        normal_vector = np.array([0, 0])
+        # since this is erroneous data, we expect the subroutine to raise a value error
+        # this allows us to then handle the error appropriately in the main program
+        # i.e. we can catch the error and display a message to the user
+        # this is better than having the program crash
+
+        with self.assertRaises(ValueError):
+            reflection_vector(
+                incident_vector, normal_vector
             )
 
 if __name__ == '__main__':
